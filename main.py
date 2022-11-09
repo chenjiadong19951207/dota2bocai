6c325c2aecabaf53030844598e4fc67dac044360
2 <c729208005@vip.qq.com>
2022-11-10 00:15:45 +0800

    完成连接设备

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-#coding=utf-8
+# -*- encoding=utf8 -*-
 from airtest.core.api import *
+
+#连接设备，具体用法&参考：https://mp.weixin.qq.com/s/znYi-eCifeMXfce9GDpW-w
+auto_setup(__file__,devices=['android://127.0.0.1:5037/emulator-5554'])
+
+start_app('com.dotamax.app')
+
